e8db019d6c3e34a95b0ad37da39d3510bd27a31a
pucara05 <pucara05@gmail.com>
2026-03-16 12:42:51 -0400

    chore: add env validation schema and env files

---
+++ src/config/env.validation.ts
@@ -0,0 +1,8 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  TELEGRAM_BOT_TOKEN: Joi.string().required(),
+  GROQ_API_KEY: Joi.string().required(),
+  NGROK_URL: Joi.string().uri().required(),
+  PORT: Joi.number().default(3000),
+});
