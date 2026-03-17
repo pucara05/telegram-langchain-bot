236e14dd3d7ffcbb227c8a19f517a12fd68cb249
pucara05 <pucara05@gmail.com>
2026-03-17 11:06:38 -0400

    chore: update main.ts and app.module.ts with config and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 1. leer las variables de entorno usando ConfigService
+  const configService = app.get(ConfigService);
+  const Port = configService.get<number>('PORT') || 3000;
+  const ngrokUrl = configService.get<string>('NGROK_URL');
+  const token = configService.get<string>('TELEGRAM_BOT_TOKEN');
+ 
+
+  // 2.registrar el webhook de Telegram automáticamente al iniciar la aplicación
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+
+
+  // 3. Registrar webhook en Telegram automáticamente
+  const webhookUrl = `${ngrokUrl}/telegram/webhook`;
+  await fetch(`https://api.telegram.org/bot${token}/setWebhook?url=${webhookUrl}`);
+  console.log(`Webhook registrado en: ${webhookUrl}`);
+
+  await app.listen(Port);
+  console.log(`Servidor escuchando en el puerto ${Port}`);
 }
 bootstrap();
